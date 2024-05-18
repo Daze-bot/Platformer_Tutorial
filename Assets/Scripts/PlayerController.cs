267a1917eb19b3ad687a379f78dccd0ad055c74e
Jevon Fillette <103719501+Daze-bot@users.noreply.github.com>
2024-05-18 03:35:31 -0400

    Add moving, running, and direction of player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,19 +5,79 @@ public class PlayerController : MonoBehaviour
 {
     Vector2 moveInput;
     public float walkSpeed = 5f;
+    public float runSpeed = 8f;
 
-    public bool IsMoving { get; private set; }
+    private bool _isMoving = false;
+    private bool _isRunning = false;
+    public bool isFacingRight = true;
+
+    public float CurrentMoveSpeed {
+        get 
+        {
+            if (IsMoving)
+            {
+                return (IsRunning) ? runSpeed : walkSpeed;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+
+    public bool IsMoving 
+    { 
+        get
+        {
+            return _isMoving;
+        } 
+        private set
+        {
+            _isMoving = value;
+            animator.SetBool("isMoving", value);
+        }
+    }
+
+    public bool IsRunning
+    {
+        get
+        {
+            return _isRunning;
+        }
+        private set
+        {
+            _isRunning = value;
+            animator.SetBool("isRunning", value);
+        }
+    }
+
+    public bool IsFacingRight { 
+        get
+        {
+            return isFacingRight;
+        }
+        private set 
+        {
+            if (isFacingRight != value)
+            {
+                transform.localScale *= new Vector2(-1, 1);
+            }
+            isFacingRight = value;
+        }
+    }
 
     Rigidbody2D rb;
+    Animator animator;  
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     private void FixedUpdate()
     {
-        rb.velocity = new Vector2(moveInput.x * walkSpeed, rb.velocity.y);
+        rb.velocity = new Vector2(moveInput.x * CurrentMoveSpeed, rb.velocity.y);
     }
 
     public void OnMove(InputAction.CallbackContext context)
@@ -25,5 +85,32 @@ public class PlayerController : MonoBehaviour
         moveInput = context.ReadValue<Vector2>();
 
         IsMoving = moveInput != Vector2.zero;
+
+        SetFacingDirection(moveInput);
+    }
+
+    private void SetFacingDirection(Vector2 moveInput)
+    {
+        if (moveInput.x > 0 && !IsFacingRight)
+        {
+            IsFacingRight = true;
+        } 
+        else if (moveInput.x < 0 && IsFacingRight)
+        {
+            IsFacingRight = false;
+        }
+    }
+
+    public void OnRun(InputAction.CallbackContext context)
+    {
+        if (context.started)
+        {
+            IsRunning = true;
+        }
+
+        else if (context.canceled)
+        {
+            IsRunning = false;
+        }
     }
 }
